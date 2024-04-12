500fc908a4bd88c768f11828f60a9ed5884c8ce7
Khushal Grover <90594296+khushalGrover@users.noreply.github.com>
2024-04-12 12:09:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'r3f-vite-starter',
   plugins: [react()],
+  server: {
+    host: true
+  },
 })
